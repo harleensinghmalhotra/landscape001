9509c234bc2fbeb394552cdc725b36e3a4f78519
harleensinghmalhotra <harleensinghmalhotra@gmail.com>
2025-11-13 01:57:55 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // ✅ Required for Vercel to render correctly
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
